0afc2128190a545c9b16a14684d05b56fe9f4652
Delhirocks <89141004+Delhirocks@users.noreply.github.com>
2023-01-27 17:52:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,19 @@
+function addUser() {
+    user_name = document.getElementById("user_name").Value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "kwitter_room.html";
+}
+
+function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+
+        });
+    });
+}
+getData();
